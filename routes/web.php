9e989f08d3267d6c305429d19ab5b8389b950993
Islam Elkady <islam.soft.eng@gmail.com>
2019-04-02 19:13:31 +0200

    admin master page setup

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+use App\Role;
+use App\User;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +21,33 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get("/getusers",function(){
+
+   $roles= Role::find(1);
+
+   foreach($roles->users as $user)
+   {
+       echo $user->name;
+   }
+
+});
+
+Route::get("/getroles",function(){
+
+    $user=User::find(1);
+//    echo "<pre>";
+//    print_r($user);
+//    echo "</pre>";
+    print_r($user->Roles()->name);
+    //foreach($user->Roles() as $role)
+    //   echo $role->name;
+    //echo $user->Roles()->name;
+});
+
+Route::get("/admin",function(){
+
+    return view("admin.index");
+
+});
+Route::resource("/admin/users","AdminUsersController");
